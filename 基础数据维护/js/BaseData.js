5950cd7f6be930c0f8cdcd8aa7d262e6ae0b26a5
michael <958659297@qq.com>
2017-07-28 17:47:00 +0800

    07/28

--- 基础数据维护/js/BaseData.js
+++ 基础数据维护/js/BaseData.js
@@ -1,25 +1,10 @@
 $(document).ready(function(){
 
-	$('input').on('click',function(){
-		$('input').css('boxShadow','');
-		$(this).css('boxShadow','1px 1px 15px 1px #71B8FB');
+	$('.div_02 .BB').click(function(){
+		$('.div_02 ._01').toggle();
 	});
 
-	$('input').keypress(function(event){
-		if(event.keyCode==13){
-			$('button').click();
-		};
-	})
 
-	$('button').click(function(){
-		if($('input').eq(0).val()==''||isNaN($('input').eq(0).val())||$('input').eq(0).val().length!=11){
-			$('a').eq(1).addClass('mobile');
-			$('a').eq(1).css('display','block');
-			$('input').eq(0).css('border','2px solid #f00');
-		}else{
-			$('a').eq(1).css('display','none');
-			$('input').eq(0).css('border','');
-		}
-	});
+
 
 });
